5a3d482c33e86d946293541764f210c923809ab5
aslakvandvik <149056256+aslakvandvik@users.noreply.github.com>
2024-10-30 15:19:09 +0100

    formatert code

--- project/models/User.py
+++ project/models/User.py
@@ -20,17 +20,28 @@ except Exception as e:
 
 # Orders a car for a customer, if the car is available
 def order_car(car_id, customer_id):
-    car = _get_connection().execute_query("MATCH (c:Car {id: $car_id}) RETURN c;", car_id=car_id)
-    customer = _get_connection().execute_query("MATCH (c:Customer {id: $customer_id}) RETURN c;", customer_id=customer_id)
+    car = _get_connection().execute_query(
+        "MATCH (c:Car {id: $car_id}) RETURN c;", car_id=car_id
+        )
+    customer = _get_connection().execute_query(
+        "MATCH (c:Customer {id: $customer_id}) RETURN c;", customer_id=customer_id
+        )
     
     # Check if the customer has already booked another car
-    existing_order = _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car) RETURN c;", customer_id=customer_id)
+    existing_order = _get_connection().execute_query(
+        "MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car) RETURN c;", customer_id=customer_id
+        )
     
     if existing_order:
         return None, "Customer has already booked another car"
     
     if car['status'] == 'available':
-        order = _get_connection().execute_query("MATCH (c:Car {id: $car_id}) ""SET c.status = 'booked' ""MERGE (customer:Customer {id: $customer_id})-[:ORDERED]->(c) ""RETURN c;", car_id=car_id, customer_id=customer_id)
+        order = _get_connection().execute_query(
+            "MATCH (c:Car {id: $car_id}) "
+            "SET c.status = 'booked' "
+            "MERGE (customer:Customer {id: $customer_id})-[:ORDERED]->(c) "
+            "RETURN c;", car_id=car_id, customer_id=customer_id
+            )
         return order, print("Car ordered by customer")
     else:
         return None, print("Car is not available")
@@ -38,10 +49,17 @@ def order_car(car_id, customer_id):
 # Cancels an order for a car by a customer
 def cancel_order_car(car_id, customer_id):
     # Check if the customer has booked the car
-    existing_order = _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car {id: $car_id}) RETURN c;", customer_id=customer_id, car_id=car_id)
+    existing_order = _get_connection().execute_query(
+        "MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car {id: $car_id}) RETURN c;", customer_id=customer_id, car_id=car_id
+        )
     
     if existing_order:
-        _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[r:ORDERED]->(c:Car {id: $car_id}) " "SET c.status = 'available' ""DELETE r " "RETURN c;", customer_id=customer_id, car_id=car_id)
+        _get_connection().execute_query(
+            "MATCH (customer:Customer {id: $customer_id})-[r:ORDERED]->(c:Car {id: $car_id}) " 
+            "SET c.status = 'available' "
+            "DELETE r " 
+            "RETURN c;", customer_id=customer_id, car_id=car_id
+            )
         return "Order cancelled and car is now available"
     else:
         return "No such order found"
@@ -49,11 +67,19 @@ def cancel_order_car(car_id, customer_id):
 # Rents a car for a customer, if the customer has a booking for the car
 def rent_car(car_id, customer_id):
     # Check if the customer has booked the car
-    existing_order = _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car {id: $car_id}) RETURN c;", customer_id=customer_id, car_id=car_id)
+    existing_order = _get_connection().execute_query(
+        "MATCH (customer:Customer {id: $customer_id})-[:ORDERED]->(c:Car {id: $car_id}) RETURN c;", 
+        customer_id=customer_id, car_id=car_id
+        )
     
     if existing_order:
         _get_connection().execute_query(
-            "MATCH (customer:Customer {id: $customer_id})-[r:ORDERED]->(c:Car {id: $car_id}) ""SET c.status = 'rented' ""DELETE r ""MERGE (customer)-[:RENTED]->(c) ""RETURN c;", customer_id=customer_id, car_id=car_id)
+            "MATCH (customer:Customer {id: $customer_id})-[r:ORDERED]->(c:Car {id: $car_id}) "
+            "SET c.status = 'rented' "
+            "DELETE r "
+            "MERGE (customer)-[:RENTED]->(c) "
+            "RETURN c;", customer_id=customer_id, car_id=car_id
+            )
         return "Car rented by customer"
     else:
         return "No booking found for this car"
@@ -61,10 +87,18 @@ def rent_car(car_id, customer_id):
 # Returns a rented car by a customer
 def return_car(car_id, customer_id, car_status):
     # Check if the customer has rented the car
-    existing_rental = _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[:RENTED]->(c:Car {id: $car_id}) RETURN c;", customer_id=customer_id, car_id=car_id)
+    existing_rental = _get_connection().execute_query(
+        "MATCH (customer:Customer {id: $customer_id})-[:RENTED]->(c:Car {id: $car_id}) RETURN c;", 
+        customer_id=customer_id, car_id=car_id
+        )
     
     if existing_rental:
-        _get_connection().execute_query("MATCH (customer:Customer {id: $customer_id})-[r:RENTED]->(c:Car {id: $car_id}) ""SET c.status = $car_status ""DELETE r ""RETURN c;", customer_id=customer_id, car_id=car_id, car_status=car_status)
+        _get_connection().execute_query(
+            "MATCH (customer:Customer {id: $customer_id})-[r:RENTED]->(c:Car {id: $car_id}) "
+            "SET c.status = $car_status "
+            "DELETE r "
+            "RETURN c;", customer_id=customer_id, car_id=car_id, car_status=car_status
+            )
         return "Car returned and status updated"
     else:
         return "No rental found for this car"
@@ -80,25 +114,35 @@ class Employee:
 def create_employee(name, adress, branch):
     new_employee = Employee(name, adress, branch)
     
-    employee = _get_connection().execute_query("MERGE (a:Employee {name: $name, adress: $adress, branch: $branch, id: $id}) RETURN a;", name=new_employee.name, adress=new_employee.adress, branch=new_employee.branch, id=new_employee.id)
+    employee = _get_connection().execute_query(
+        "MERGE (a:Employee {name: $name, adress: $adress, branch: $branch, id: $id}) RETURN a;", 
+        name=new_employee.name, adress=new_employee.adress, branch=new_employee.branch, id=new_employee.id
+        )
     
     return employee, print("employee added to database")
 
 # Get employee from database
 def read_employee(employee_id):
-    employee = _get_connection().execute_query("MATCH (a:Employee {id: $id}) RETURN a;", id=employee_id)
+    employee = _get_connection().execute_query(
+        "MATCH (a:Employee {id: $id}) RETURN a;", id=employee_id
+        )
     return employee, print("employee found in database")
     
 
 # Update existing values for employee in Database
 def update_employee(employee_id, name, adress, branch):
-    employee = _get_connection().execute_query("MATCH (a:Employee {id: $id}) SET a.name = $name, a.adress = $adress, a.branch = $branch RETURN a;", id=employee_id, name=name, adress=adress, branch=branch)
+    employee = _get_connection().execute_query(
+        "MATCH (a:Employee {id: $id}) SET a.name = $name, a.adress = $adress, a.branch = $branch RETURN a;", 
+        id=employee_id, name=name, adress=adress, branch=branch
+        )
     return employee, print("employee updated in database")
 
 
 # Delete Employee from database
 def delete_employee(employee_id):
-    employee = _get_connection().execute_query("MATCH (a:Employee {id: $id}) DELETE a;", id=employee_id)
+    employee = _get_connection().execute_query(
+        "MATCH (a:Employee {id: $id}) DELETE a;", id=employee_id
+        )
     return employee, print("employee deleted from database")
     
 
@@ -117,25 +161,34 @@ class Car:
 def create_car(brand, model, year, location):
     new_car = Car(brand, model, year, location)
 
-    car = _get_connection().execute_query("MERGE (a:Car {year: $year, location: $location, model: $model, id: $id, brand: $brand, status: $status}) RETURN a;", year=new_car.year, location=new_car.location, model=new_car.model, id=new_car.id, brand=new_car.brand, status=new_car.status)
+    car = _get_connection().execute_query(
+        "MERGE (a:Car {year: $year, location: $location, model: $model, id: $id, brand: $brand, status: $status}) RETURN a;", 
+        year=new_car.year, location=new_car.location, model=new_car.model, id=new_car.id, brand=new_car.brand, status=new_car.status
+        )
 
     return car, print("car added to database")
 
 # Get car from database by using car id
 def read_car(self):
-    car = _get_connection().execute_query("MATCH (a:Car {id: $id}) RETURN a;", id=self.id)
+    car = _get_connection().execute_query(
+        "MATCH (a:Car {id: $id}) RETURN a;", id=self.id
+        )
     return car, print("car found in database")
    
 # Update existing values for car in Database by searching for the car id
 def update_car(id, brand, model, year, location, status):
-    car = _get_connection().execute_query("MATCH (a:Car {id: $id}) SET a.brand = $brand, a.model = $model, a.year = $year, a.location = $location , a.status = $status RETURN a;", id=id, brand=brand, model=model, year=year, location=location, status=status)
+    car = _get_connection().execute_query(
+        "MATCH (a:Car {id: $id}) SET a.brand = $brand, a.model = $model, a.year = $year, a.location = $location , a.status = $status RETURN a;", 
+        id=id, brand=brand, model=model, year=year, location=location, status=status
+        )
     return car, print("car updated in database")
 
 # Delete car from database by using car id
 def delete_car(id):
-    car = _get_connection().execute_query("MATCH (a:Car {id: $id}) DELETE a;", id=id)
+    car = _get_connection().execute_query(
+        "MATCH (a:Car {id: $id}) DELETE a;", id=id
+        )
     return car, print("car deleted from database")
-    
     
 class Customer:
     
@@ -147,27 +200,36 @@ class Customer:
 
 # Create a new Customer and add to database
 def create_customer(name, age, adress):
-    
+
     new_customer = Customer(name, age, adress)
     
-    customer = _get_connection().execute_query("MERGE (c:CUSTOMER {adress: $adress, name: $name, id: $id, age: $age}) RETURN c;", adress=new_customer.adress, name=new_customer.name, age=new_customer.age)
+    customer = _get_connection().execute_query(
+        "MERGE (c:CUSTOMER {adress: $adress, name: $name, id: $id, age: $age}) RETURN c;", 
+        adress=new_customer.adress, name=new_customer.name, age=new_customer.age
+        )
     return customer, print("customer added to database")
 
 # Get Customer from database
 def read_customer(id):
-    customer = _get_connection().execute_query("MATCH (c:Customer {id: $id}) RETURN c;", id=id)
+    customer = _get_connection().execute_query(
+        "MATCH (c:Customer {id: $id}) RETURN c;", id=id
+        )
     return customer, print("customer found in database")
 
 # Update existing values for Customer in Database
 def update_customer(id, name, age, adress):
-    customer = _get_connection().execute_query("MATCH (c:Customer {id: $id}) SET c.name = $name, c.age = $age, c.adress = $adress RETURN c;", id=id, name=name, age=age, adress=adress)
+    customer = _get_connection().execute_query(
+        "MATCH (c:Customer {id: $id}) SET c.name = $name, c.age = $age, c.adress = $adress RETURN c;", 
+        id=id, name=name, age=age, adress=adress
+        )
     return customer, print("customer updated in database")
 
 # Delete Customer from database
 def delete_customer(id):
-    customer = _get_connection().execute_query("MATCH (c:Customer {id: $id}) DELETE c;", id=id)
+    customer = _get_connection().execute_query(
+        "MATCH (c:Customer {id: $id}) DELETE c;", id=id
+        )
     return customer, print("customer deleted from database")
-
 
 # Close the connection
 driver.close()
