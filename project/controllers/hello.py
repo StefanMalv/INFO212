a50e2f70d6c31f4335ca59db978f81ea368b5d5a
LazyTurtle <123999336+StefanMalv@users.noreply.github.com>
2024-10-30 15:25:32 +0100

    endringer til hello.py

--- project/controllers/hello.py
+++ project/controllers/hello.py
@@ -1,25 +1,53 @@
-from project import app
+from project.templates import app
 from flask import render_template, request, redirect, url_for
+import project.models.User as user
 
-#route index
-@app.route('/', methods=["GET", "POST"])
-def index():
 
+@app.route("/order_car", methods=["POST"])
+def index_order_car():
+    car_id = request.form["car_id"]
+    customer_id = request.form["customer_id"]
+    
     if request.method == "POST":
-        username = request.form["username"]
+        try:   
+           user.order_car(car_id, customer_id)
+           
+        except:
+                return "Could not order car"
+
+@app.route("/cancel_car", methods=["PUT"])
+def index_cancel_order():
+    cancel_car_id = request.form["car_id"]
+    cancel_customer_id = request.form["customer_id"]
+    
+    if request.method == "PUT":
+        try:   
+           user.cancel_order_car(cancel_car_id, cancel_customer_id)
+           
+        except:
+                return "Could not cancel order"
+            
+@app.route("/rent_car", methods=["POST"])
+def index_rent_car():
+    rent_car_id = request.form["car_id"]
+    rent_customer_id = request.form["customer_id"]
+    
+    if request.method == "POST":
+        try:    
+            user.rent_car(rent_car_id, rent_customer_id)
+        
+        except:
+            return "Could not rent car"
+        
+@app.route("/return_car", methods=["POST"])
+def index_return_car():
+    rent_car_id = request.form["car_id"]
+    rent_customer_id = request.form["customer_id"]
+    
+    if request.method == "POST":
         try:
-            user = findUserByUsername(username)
-            data = {
-                "username": user.username,
-                "email": user.email
-            }
-        except err:
-            print (err)
-
-    else:
-        data = {
-            "username": "Not specified",
-            "email": "Not specified"
-        }
-    return render_template('index.html.j2', data = data)
-
+            user.return_car(rent_car_id, rent_customer_id)
+        
+        except:
+            return "Could not return car"
+        
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #Project Flask MVC
 
-from project import app
+from project.templates import app
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
